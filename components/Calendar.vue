564d79499776b706932fb0b40e8210429dac0404
amos101 <calcio.vita@gmail.com>
2022-06-16 13:21:00 +0900

    fix

---
+++ components/Calendar.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th id="prev">&laquo;</th>
+          <th id="title" colspan="5">2020/05</th>
+          <th id="next">&raquo;</th>
+        </tr>
+        <tr>
+          <th>Sun</th>
+          <th>Mon</th>
+          <th>Tue</th>
+          <th>Wed</th>
+          <th>Thu</th>
+          <th>Fri</th>
+          <th>Sat</th>
+        </tr>
+      </thead>
+      <tbody>
+      </tbody>
+      <tfoot>
+        <tr>
+          <td id="today" colspan="7">Today</td>
+        </tr>
+      </tfoot>
+    </table>
+  </div>
+</template>
+<script>
+  const today = new Date();
+  let year = today.getFullYear();
+  let month = today.getMonth();
+export default ({
+  })
+</script>
+<style scoped>
+body {
+  font-family: 'Courier New', monospace;
+  font-size: 14px;
+}
+
+table {
+  border-collapse: collapse;
+  border: 2px solid #eee;
+}
+
+thead,
+tfoot {
+  background: #eee;
+}
+
+th,
+td {
+  padding: 8px;
+  text-align: center;
+}
+
+tbody td:first-child {
+  color: red;
+}
+
+tbody td:last-child {
+  color: blue;
+}
+
+tfoot {
+  font-weight: bold;
+}
+
+td.disabled {
+  opacity: 0.3;
+}
+
+td.today {
+  font-weight: bold;
+}
+
+#prev,
+#next,
+#today {
+  cursor: pointer;
+  user-select: none;
+}
+</style>
---
+++ pages/calendar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Calendar />
+  </div>
+</template>
+
+<script>
+import Calendar from '../components/Calendar.vue'
+
+export default {
+  name: 'IndexPage',
+  components: {
+    Calendar
+  },
+}
+</script>
